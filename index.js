8d6507633ceb273f535827bf4df3fa3a6b05364a
Gregor MacLennan <gmaclennan@digital-democracy.org>
2022-06-01 22:45:57 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,240 @@
+const { TypedEmitter } = require('tiny-typed-emitter')
+
+/**
+ * @private
+ * @typedef {'stopped' | 'starting' | 'started' | 'stopping' | 'error'} ServiceStateValue
+ */
+/**
+ * @typedef {{ value: Exclude<ServiceStateValue, 'error'> } | { value: 'error', error: Error }} ServiceState
+ */
+
+/**
+ * @private
+ * @typedef {Object} InternalEvents
+ * @property {() => void} started
+ * @property {() => void} stopped
+ * @property {(error: Error) => void} internal-error
+ */
+
+/**
+ * @typedef {Object} ExternalEvents
+ * @property {(state: ServiceState) => void} state
+ */
+
+/**
+ * A state machine for managing a service that has asynchronous "start" and
+ * "stop" methods. Create an instance passing async `opts.start()` and
+ * `opts.stop()` methods. It manages state following some basic rules:
+ *
+ * - Most importantly: You can call start() and stop() multiple times, but the
+ *   service will end in the state of the last call (e.g. if the last call was
+ *   to `stop()` then it will end up stopped)
+ * - Calling `start()` when the service is "stopped" calls the `opts.start()` method
+ *   and resolves when it completes.
+ * - Calling `start()` when the service is "starting" (e.g. `start()` has been
+ *   called but has not completed) will not call `opts.start()` again, but will
+ *   resolve once the service has started
+ * - Calling `start()` when the service is "started" will resolve immediately
+ *   and do nothing.
+ * - If `opts.start()` or `opts.stop()` throw, then the service is left in an
+ *   unrecoverable "error" state.
+ * - Calling `start()` or `stop()` when the service is in "error" state will
+ *   throw with the error from the error state
+ *
+ * Logic for calling `stop()` follows the inverse of `start()`.
+ *
+ *
+ *
+ * To wait for the service to be in the "started" state from other methods, use
+ * `await stateMachine.started()`. Note that if the services is "stopping" or
+ * "stopped" then this will await (e.g. queue) until next start
+ *
+ * @extends {TypedEmitter<ExternalEvents>}
+ */
+class StartStopStateMachine extends TypedEmitter {
+  /** @type {ServiceState} */
+  #state = { value: 'stopped' }
+  /** @type {TypedEmitter<InternalEvents>} */
+  #emitter = new TypedEmitter()
+  #start
+  #stop
+
+  /**
+   * @param {Object} [opts]
+   * @param {(...args: any[]) => Promise<void>} [opts.start]
+   * @param {() => Promise<void>} [opts.stop]
+   */
+  constructor({ start = async () => {}, stop = async () => {} } = {}) {
+    super()
+    this.#start = start
+    this.#stop = stop
+  }
+
+  get state() {
+    return this.#state
+  }
+
+  /**
+   * @private
+   * @param {ServiceState} state
+   */
+  _setState(state) {
+    this.#state = state
+    if (state.value === 'started') this.#emitter.emit('started')
+    else if (state.value === 'stopped') this.#emitter.emit('stopped')
+    else if (state.value === 'error')
+      this.#emitter.emit('internal-error', state.error)
+    this.emit('state', state)
+  }
+
+  /**
+   * Will resolve when the service is in started state. E.g. to ensure an async
+   * method only runs when the service is in "started" state, use:
+   *
+   * ```js
+   * await this.started()
+   * ```
+   *
+   * Will reject if the service is in "error" state.
+   *
+   * Note: If the service is in "stopping" or "stopped" state this will queue
+   * until the next time the service starts. If this is not desirable behaviour,
+   * check this.#state.value first
+   *
+   * @returns {Promise<void>}
+   */
+  async started() {
+    if (this.#state.value === 'started') return
+    if (this.#state.value === 'error') throw this.#state.error
+    const emitter = this.#emitter
+    return new Promise((resolve, reject) => {
+      emitter.once('started', onStarted)
+      emitter.once('internal-error', onError)
+      function onStarted() {
+        emitter.off('internal-error', onError)
+        resolve()
+      }
+      /**
+       * @private
+       * @param {Error} err
+       */
+      function onError(err) {
+        emitter.off('started', onStarted)
+        reject(err)
+      }
+    })
+    /* c8 ignore next */
+  }
+
+  /**
+   * Will resolve when the service is in stopped state. Less useful than
+   * `started()` E.g. to ensure an async method only runs when the service is in
+   * "stopped" state, use:
+   *
+   * ```js
+   * await this.stopped()
+   * ```
+   *
+   * Will reject if the service is in "error" state.
+   *
+   * Note: If the service is in "starting" or "started" state this will queue
+   * until the next time the service stops. If this is not desirable behaviour,
+   * check this.#state.value first
+   *
+   * @returns {Promise<void>}
+   */
+  async stopped() {
+    if (this.#state.value === 'stopped') return
+    if (this.#state.value === 'error') throw this.#state.error
+    const emitter = this.#emitter
+    return new Promise((resolve, reject) => {
+      emitter.once('stopped', onStopped)
+      emitter.once('internal-error', onError)
+      function onStopped() {
+        emitter.off('internal-error', onError)
+        resolve()
+      }
+      /**
+       * @private
+       * @param {Error} err
+       */
+      function onError(err) {
+        emitter.off('stopped', onStopped)
+        reject(err)
+      }
+    })
+    /* c8 ignore next */
+  }
+
+  /**
+   * Start service. If the service is starting or started, will resolve when the
+   * service is started, and will not call opts.start() for than once. If the
+   * service is in the process of stopping, will wait until it stops before
+   * starting and will not call opts.stop() more than once
+   *
+   * @param {any[]} args
+   * @returns {Promise<void>} Resolves when service is started
+   */
+  async start(...args) {
+    switch (this.#state.value) {
+      case 'starting':
+        await this.started()
+        // Avoid race condition if another function is queued up
+        return this.start(...args)
+      case 'started':
+        return
+      case 'error':
+        return Promise.reject(this.#state.error)
+      case 'stopping':
+        // Wait until stopped before continuing
+        await this.stopped()
+        // Avoid race condition if another function is queued up
+        return this.start(...args)
+      case 'stopped':
+      default:
+      // Continue
+    }
+    try {
+      this._setState({ value: 'starting' })
+      await this.#start.apply(this, args)
+      this._setState({ value: 'started' })
+    } catch (e) {
+      this._setState({ value: 'error', error: e })
+      throw e
+    }
+  }
+
+  /**
+   * Stop the service.
+   *
+   * @returns {Promise<void>}
+   */
+  async stop() {
+    switch (this.#state.value) {
+      case 'stopping':
+        await this.stopped()
+        return this.stop()
+      case 'stopped':
+        return
+      case 'error':
+        return Promise.reject(this.#state.error)
+      case 'starting':
+        // Wait until started until stopping
+        await this.started()
+        return this.stop()
+      case 'started':
+      default:
+      // Continue
+    }
+    try {
+      this._setState({ value: 'stopping' })
+      await this.#stop()
+      this._setState({ value: 'stopped' })
+    } catch (e) {
+      this._setState({ value: 'error', error: e })
+      throw e
+    }
+  }
+}
+
+module.exports = StartStopStateMachine
---
+++ test.js
@@ -0,0 +1,417 @@
+const test = require('tape')
+const StartStopStateMachine = require('./')
+
+// * - Calling `start()` when the service is "stopped" calls the `opts.start()` method
+// *   and resolves when it completes.
+// * - Calling `start()` when the service is "starting" (e.g. `start()` has been
+// *   called but has not completed) will not call `opts.start()` again, but will
+// *   resolve once the service has started
+// * - Calling `start()` when the service is "started" will resolve immediately
+// *   and do nothing.
+// * - If `opts.start()` or `opts.stop()` throw, then the service is left in an
+// *   unrecoverable "error" state.
+// * - Calling `start()` or `stop()` when the service is in "error" state will
+// *   throw with the error from the error state
+
+test('Calling `start()` when the service is "stopped" calls the `opts.start()` method and resolves when it completes.', async (t) => {
+  let started = false
+  const service = new StartStopStateMachine({
+    async start() {
+      await new Promise((res) => setTimeout(res, 100))
+      started = true
+    },
+  })
+  await service.start()
+  t.true(started, 'Service is started')
+  t.deepEqual(service.state, { value: 'started' })
+})
+
+test('Calling `start()` when the service is "starting" (e.g. `start()` has been called but has not completed) will not call `opts.start()` again, but will resolve once the service has started.', async (t) => {
+  let startCount = 0
+  const service = new StartStopStateMachine({
+    async start() {
+      await new Promise((res) => setTimeout(res, 100))
+      startCount++
+    },
+  })
+  t.deepEqual(
+    service.state,
+    { value: 'stopped' },
+    'Service is in stopped state'
+  )
+  service.start()
+  service.start()
+  await new Promise((res) => setTimeout(res, 0))
+  t.equal(startCount, 0, 'opts.start() has not yes been called')
+  t.deepEqual(
+    service.state,
+    { value: 'starting' },
+    'Service is in starting state'
+  )
+  await service.start()
+  t.equal(startCount, 1, 'service opts.start() is only called once')
+  t.deepEqual(
+    service.state,
+    { value: 'started' },
+    'Service is in started state'
+  )
+})
+
+test('Calling `start()` when the service is "started" will resolve immediately and do nothing.', async (t) => {
+  let startCount = 0
+  const service = new StartStopStateMachine({
+    async start() {
+      await new Promise((res) => setTimeout(res, 100))
+      startCount++
+    },
+  })
+  await service.start()
+  t.equal(startCount, 1, 'service has started')
+  t.deepEqual(
+    service.state,
+    { value: 'started' },
+    'Service is in started state'
+  )
+  const timeStart = Date.now()
+  await service.start()
+  t.true(
+    Date.now() - timeStart < 10,
+    'calling second time resolves immediately'
+  )
+  await new Promise((res) => setTimeout(res, 0))
+  t.equal(startCount, 1, 'service opts.start() has only been called once')
+})
+
+test('If `opts.start()` throws, then the service is left in an unrecoverable "error" state.', async (t) => {
+  let startCount = 0
+  const service = new StartStopStateMachine({
+    async start() {
+      await new Promise((res) => setTimeout(res, 100))
+      startCount++
+      // Throw the first time, but not on subsequent calls, but this should
+      // never be called after it has thrown once
+      if (startCount === 1) throw new Error('MyError')
+    },
+  })
+  try {
+    await service.start()
+  } catch (e) {
+    t.ok(e instanceof Error, 'throws on first call')
+  }
+  t.equal(service.state.value, 'error', 'in error state')
+  try {
+    await service.start()
+  } catch (e) {
+    t.ok(
+      e instanceof Error,
+      'throws once in error state, even if opts.start() does not throw on second call'
+    )
+  }
+  t.equal(service.state.value, 'error', 'in error state')
+})
+
+test('Calling `start()` or `stop()` when the service is in "error" state will throw with the error from the error state', async (t) => {
+  let startCount = 0
+  const testError = new Error('TestError')
+  const service = new StartStopStateMachine({
+    async start() {
+      startCount++
+      return Promise.reject(testError)
+    },
+  })
+  try {
+    await service.start()
+    t.fail('should not reach here')
+  } catch (e) {
+    t.equal(e, testError, 'start() throws with error from error state')
+  }
+  try {
+    await service.start()
+    t.fail('should not reach here')
+  } catch (e) {
+    t.equal(e, testError, 'start() throws with error from error state')
+  }
+  try {
+    await service.stop()
+    t.fail('should not reach here')
+  } catch (e) {
+    t.equal(e, testError, 'stop() throws with error from error state')
+  }
+  t.equal(startCount, 1, 'Only called opts.start() once when first threw')
+  t.deepEqual(service.state, { value: 'error', error: testError })
+})
+
+test('Multiple calls to `start()` or `stop()` when the service is in "error" state will throw the same error', async (t) => {
+  let stopCount = 0
+  const testError = new Error('TestError')
+  const service = new StartStopStateMachine({
+    async start() {
+      return Promise.reject(testError)
+    },
+    async stop() {
+      stopCount++
+    },
+  })
+
+  for (let i = 0; i < 10; i++) {
+    try {
+      await service.start()
+      t.fail('should not reach here')
+    } catch (e) {
+      t.equal(e, testError, 'start() throws with error from error state')
+    }
+  }
+  for (let i = 0; i < 10; i++) {
+    try {
+      await service.stop()
+      t.fail('Should not reach here')
+    } catch (e) {
+      t.equal(e, testError, 'stop() throws with error from error state')
+    }
+  }
+  t.equal(stopCount, 0, 'opts.stop() is never called due to error state')
+  t.deepEqual(service.state, { value: 'error', error: testError })
+})
+
+test('Awaiting started() when service is starting, but subsequently errors, throws', async (t) => {
+  const testError = new Error('TestError')
+  const service = new StartStopStateMachine({
+    async start() {
+      await new Promise((res) => process.nextTick(res))
+      return Promise.reject(testError)
+    },
+  })
+  const startPromise = service
+    .start()
+    .catch((e) =>
+      t.equal(e, testError, 'start() throws with error from error state')
+    )
+  t.equal(service.state.value, 'starting', 'in starting state')
+  try {
+    await service.started()
+    t.fail('should not reach here')
+  } catch (e) {
+    t.equal(e, testError, 'started() throws with error from error state')
+  }
+  await startPromise
+})
+
+test('Awaiting stopped() when service is stopping, but subsequently errors, throws', async (t) => {
+  const testError = new Error('TestError')
+  const service = new StartStopStateMachine({
+    async stop() {
+      await new Promise((res) => process.nextTick(res))
+      return Promise.reject(testError)
+    },
+  })
+  await service.start()
+  const stopPromise = service
+    .stop()
+    .catch((e) =>
+      t.equal(e, testError, 'stop() throws with error from error state')
+    )
+  t.equal(service.state.value, 'stopping', 'in stopping state')
+  try {
+    await service.stopped()
+    t.fail('should not reach here')
+  } catch (e) {
+    t.equal(e, testError, 'stopped() throws with error from error state')
+  }
+  await stopPromise
+})
+
+test('Awaiting started() when service is starting, resolves when the service is started', async (t) => {
+  const service = new StartStopStateMachine({
+    async start() {
+      await new Promise((res) => process.nextTick(res))
+    },
+  })
+  const startPromise = service.start()
+  t.equal(service.state.value, 'starting', 'in starting state')
+  await service.started()
+  t.equal(service.state.value, 'started', 'in started state')
+  await startPromise
+})
+
+test('Awaiting stopped() when service is stopping, resolves when the service is stopped', async (t) => {
+  const service = new StartStopStateMachine({
+    async stop() {
+      await new Promise((res) => process.nextTick(res))
+    },
+  })
+  await service.start()
+  const stopPromise = service.stop()
+  t.equal(service.state.value, 'stopping', 'in stopping state')
+  await service.stopped()
+  t.equal(service.state.value, 'stopped', 'in stopped state')
+  await stopPromise
+})
+
+test('Awaiting `started()` or `stopped()` when the service is in "error" state will throw with the error from the error state', async (t) => {
+  const testError = new Error('TestError')
+  const service = new StartStopStateMachine({
+    async start() {
+      return Promise.reject(testError)
+    },
+  })
+  try {
+    await service.start()
+    t.fail('should not reach here')
+  } catch (e) {
+    t.equal(e, testError, 'start() throws with error from error state')
+  }
+  try {
+    await service.started()
+    t.fail('should not reach here')
+  } catch (e) {
+    t.equal(e, testError, 'started() throws with error from error state')
+  }
+  try {
+    await service.stopped()
+    t.fail('should not reach here')
+  } catch (e) {
+    t.equal(e, testError, 'stopped() throws with error from error state')
+  }
+})
+
+test('Awaiting started() when service is started resolves', async (t) => {
+  const service = new StartStopStateMachine()
+  await service.start()
+  t.equal(service.state.value, 'started', 'in started state')
+  await service.started()
+  t.pass('started() resolves')
+})
+
+test('Awaiting stopped() when service is stopped resolves', async (t) => {
+  const service = new StartStopStateMachine()
+  await service.start()
+  await service.stop()
+  t.equal(service.state.value, 'stopped', 'in stopped state')
+  await service.stopped()
+  t.pass('stopped() resolves')
+})
+
+test('Calling `stop()` when the service is "started" calls the `opts.stop()` method and resolves when it completes.', async (t) => {
+  let started = false
+  const service = new StartStopStateMachine({
+    async start() {
+      started = true
+    },
+    async stop() {
+      started = false
+    },
+  })
+  await service.start()
+  t.true(started, 'Service is in started state')
+  t.deepEqual(service.state, { value: 'started' })
+  await service.stop()
+  t.false(started, 'Service is stopped once `stop()` resolves')
+  t.deepEqual(service.state, { value: 'stopped' })
+})
+
+test('Calling `stop()` when the service is "stopping" (e.g. `stop()` has been called but has not completed) will not call `opts.stop()` again, but will resolve once the service has stopped.', async (t) => {
+  let stopCount = 0
+  const service = new StartStopStateMachine({
+    async stop() {
+      await new Promise((res) => setTimeout(res, 100))
+      stopCount++
+    },
+  })
+  await service.start()
+  service.stop()
+  service.stop()
+  await new Promise((res) => setTimeout(res, 0))
+  t.equal(stopCount, 0, 'Service not yet stopped')
+  t.deepEqual(service.state, { value: 'stopping' })
+  await service.stop()
+  t.equal(stopCount, 1, 'service opts.stop() is only called once')
+  t.deepEqual(service.state, { value: 'stopped' })
+})
+
+test('Calling `stop()` when the service is "stopped" will resolve immediately and do nothing.', async (t) => {
+  let stopCount = 0
+  const service = new StartStopStateMachine({
+    async stop() {
+      await new Promise((res) => setTimeout(res, 100))
+      stopCount++
+    },
+  })
+  await service.start()
+  await service.stop()
+  t.equal(stopCount, 1, 'service has stopped')
+  t.deepEqual(service.state, { value: 'stopped' })
+  const timeStart = Date.now()
+  await service.stop()
+  t.true(
+    Date.now() - timeStart < 10,
+    'calling second time resolves immediately'
+  )
+  await new Promise((res) => setTimeout(res, 0))
+  t.equal(stopCount, 1, 'service opts.stop() has only been called once')
+  t.deepEqual(service.state, { value: 'stopped' })
+})
+
+test('If `opts.stop()` throws, then the service is left in an unrecoverable "error" state.', async (t) => {
+  let stopCount = 0
+  const service = new StartStopStateMachine({
+    async stop() {
+      await new Promise((res) => setTimeout(res, 20))
+      stopCount++
+      // Throw the first time, but not on subsequent calls, but this should
+      // never be called after it has thrown once
+      if (stopCount === 1) throw new Error('MyError')
+    },
+  })
+  await service.start()
+  try {
+    await service.stop()
+    t.fail('Should not reach here')
+  } catch (e) {
+    t.ok(e instanceof Error, 'throws on first call')
+  }
+  t.equal(service.state.value, 'error', 'in error state')
+  try {
+    await service.stop()
+  } catch (e) {
+    t.ok(
+      e instanceof Error,
+      'throws once in error state, even if opts.start() does not throw on second call'
+    )
+  }
+  t.equal(service.state.value, 'error', 'in error state')
+})
+
+test('Calling start() and stop() multiple times ends in expected state', async (t) => {
+  let started = false
+  const service = new StartStopStateMachine({
+    async start() {
+      await new Promise((res) => setTimeout(res, 60))
+      started = true
+    },
+    async stop() {
+      await new Promise((res) => setTimeout(res, 20))
+      started = false
+    },
+  })
+  service.start()
+  await nextTick()
+  service.start()
+  await nextTick()
+  service.stop()
+  await nextTick()
+  await service.start()
+  t.true(started, 'service is started')
+  t.deepEqual(service.state, { value: 'started' })
+
+  service.stop()
+  await nextTick()
+  service.start()
+  await nextTick()
+  await service.stop()
+  t.false(started, 'service is stopped')
+  t.deepEqual(service.state, { value: 'stopped' })
+})
+
+async function nextTick() {
+  return new Promise((res) => process.nextTick(res))
+}
